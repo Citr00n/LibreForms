33b7a46fa4ec276dc7ecb8e23f1aadff497c54e7
Restyled.io <commits@restyled.io>
2024-04-06 23:03:01 +0000

    Restyled by isort

--- LibreForms/urls.py
+++ LibreForms/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 from utils.views import *
 
 urlpatterns = [
