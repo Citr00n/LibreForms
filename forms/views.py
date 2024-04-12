105d91ee33ec6d1c1ea6051c5aff82296e2d8515
Restyled.io <commits@restyled.io>
2024-04-12 22:17:19 +0000

    Restyled by reorder-python-imports

--- forms/views.py
+++ forms/views.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
 from .models import *
 
