b4a4b1a029cda7246d2d92cecbf2a56bb9d1d0e6
Citr0n <citroncorp@mail.ru>
2024-04-14 20:54:33 +0300

    Финальные доработки

--- user/views.py
+++ user/views.py
@@ -65,7 +65,7 @@ def logout_view(req):
     if req.user.is_authenticated is True:
         if req.method == "GET":
             return render(req, "logout.html", context={"title": "Выход"})
-        elif req.method == "POST":
+        elif req.method == "POST" and req.path != "/admin/logout":
             logout(req)
             return redirect("login")
     else:
