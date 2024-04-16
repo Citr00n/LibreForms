5c5c3e8183c80562bccc2afa3fb01392143f5581
Restyled.io <commits@restyled.io>
2024-04-16 08:53:27 +0000

    Restyled by reorder-python-imports

--- forms/views.py
+++ forms/views.py
@@ -3,7 +3,8 @@ from collections import Counter
 
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from plotly.subplots import make_subplots
 
 from .models import *
