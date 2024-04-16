167bae3ecef8398ab7fff0cd9aa1c1fbaa6abf95
Restyled.io <commits@restyled.io>
2024-04-16 08:53:11 +0000

    Restyled by black

--- user/views.py
+++ user/views.py
@@ -25,12 +25,7 @@ def login_view(req):
 
     elif req.method == "GET":
         form = forms.UserForm()
-        return render(req,
-                      "login.html",
-                      context={
-                          "title": "Вход",
-                          "form": form
-                      })
+        return render(req, "login.html", context={"title": "Вход", "form": form})
 
     elif req.method == "POST":
         username = req.POST["username"]
@@ -50,9 +45,7 @@ def home_view(req):
 
     """
     if req.user.is_authenticated is True:
-        return render(req,
-                      "userhome.html",
-                      context={"title": req.user.username})
+        return render(req, "userhome.html", context={"title": req.user.username})
     else:
         return redirect("login")
 
@@ -82,12 +75,9 @@ def signup_view(req):
     if req.user.is_authenticated is not True:
         if req.method == "GET":
             form = forms.UserForm()
-            return render(req,
-                          "signup.html",
-                          context={
-                              "title": "Регистрация",
-                              "form": form
-                          })
+            return render(
+                req, "signup.html", context={"title": "Регистрация", "form": form}
+            )
 
         elif req.method == "POST":
             if not list(User.objects.all()):
