d57a65927deb3fab54ce2852bcc635fec547e393
Citr0n <citroncorp@mail.ru>
2024-04-17 01:18:58 +0300

    Попытка подключить БД

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
+pip install gunicorn fcntl
 # Convert static asset files
 python manage.py collectstatic --no-input
 
