42b05ba8c2ca6c0154cbbe22bfad0b22e45d9716
Restyled.io <commits@restyled.io>
2024-04-16 08:53:57 +0000

    Restyled by reorder-python-imports

--- LibreForms/urls.py
+++ LibreForms/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include
+from django.urls import path
 
 from user.views import *
 from utils.views import *
