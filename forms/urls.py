ae3544ac4dfef7b703c980c406b3f056fda85030
Citr0n <citroncorp@mail.ru>
2024-04-24 20:07:19 +0300

    Фикс ошибки с URL аналитики

--- forms/urls.py
+++ forms/urls.py
@@ -6,7 +6,7 @@ from .views import *
 
 urlpatterns = [
     path("<uuid:form_id>/", form_view, name="form"),
-    path("<uuid:form_id>/analytics", analytics_view, name="analytics"),
+    path("<uuid:form_id>/analytics/", analytics_view, name="analytics"),
     path("<uuid:form_id>/export/", exports_view, name="exports"),
     path("<uuid:form_id>/export/<uuid:question_id>", export_view, name="question_export"),
 ]
